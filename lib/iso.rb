f6233b28b9b43427356a05d785c4df68d636101f
GARY <gary.v.ho@gmail.com>
2020-03-26 15:05:08 +1100

    added some ascii

--- lib/iso.rb
+++ lib/iso.rb
@@ -1,6 +1,7 @@
 require 'tty-spinner'
 require 'tty-prompt'
-
+require 'artii'
+a = Artii::Base.new 
 prompt = TTY::Prompt.new
 choices = %w(push-ups sit-ups crunches pull-ups back-flips)
 work_out_picks = prompt.multi_select("Please select your workouts", choices)
@@ -22,17 +23,20 @@ elsif work_out_dif[0] == "Medium"
     sets = 2
 else
     sets = 3
-    p "no"
 end
 
 
 def timer(exercise, sec, rest)
-
+    b = Artii::Base.new 
     for i in 0..exercise.length - 1
+        puts b.asciify("#{exercise[i].upcase}")
+
         spinner = TTY::Spinner.new("[:spinner] #{exercise[i]}", format: :pulse_2)
         spinner.auto_spin
         sleep(sec.to_i)
         spinner.success('Done!')
+
+        puts b.asciify('REST : )')
 
         if rest == "Easy"
             rest(30)
@@ -53,6 +57,7 @@ def rest(sec)
 end
 
 for i in 0..sets - 1
+    puts a.asciify("SET #{i+1}")
     puts "Set #{i+1}"
     timer(work_out_picks, work_out_length, work_out_dif)
 end
