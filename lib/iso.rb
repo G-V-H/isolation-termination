fc5bbbe52e73a0bbbc4e7d911cfddefcfaa62734
GARY <gary.v.ho@gmail.com>
2020-03-26 15:51:46 +1100

    additional ascii

--- lib/iso.rb
+++ lib/iso.rb
@@ -1,7 +1,7 @@
 require 'tty-spinner'
 require 'tty-prompt'
 require 'artii'
-a = Artii::Base.new 
+
 prompt = TTY::Prompt.new
 choices = %w(push-ups sit-ups crunches pull-ups back-flips)
 work_out_picks = prompt.multi_select("Please select your workouts", choices)
@@ -9,8 +9,8 @@ work_out_picks = prompt.multi_select("Please select your workouts", choices)
 #prompt user for length of time in seconds for each exercise
 work_out_length = prompt.ask("How many seconds would you like to do for each exercise?") do |q|
     q.required true
-    q.validate /\A[0-9]+\Z/ 
-    q.modify :int
+    q.validate(/\A[0-9]+\Z/, 'Not a number')
+    q.convert :int
   end
 #prompt user for difficulty (easy, medium, hard) will set rest time (30 1, 20 2, 10 3)
 
@@ -26,21 +26,21 @@ else
 end
 
 
-def timer(exercise, sec, rest)
-    b = Artii::Base.new 
+def timer(exercise, sec, rester)
+    
     for i in 0..exercise.length - 1
-        puts b.asciify("#{exercise[i].upcase}")
+        ascii_art("#{exercise[i].upcase}")
 
         spinner = TTY::Spinner.new("[:spinner] #{exercise[i]}", format: :pulse_2)
         spinner.auto_spin
-        sleep(sec.to_i)
+        sleep(sec)
         spinner.success('Done!')
 
-        puts b.asciify('REST : )')
+        ascii_art("REST : )")
 
-        if rest == "Easy"
+        if rester == "Easy"
             rest(30)
-        elsif rest == "Medium"
+        elsif rester == "Medium"
             rest(20)
         else
             rest(10)
@@ -55,9 +55,18 @@ def rest(sec)
     sleep(sec)
     rester.success('Done!')
 end
+def ascii_art(art)
+    b = Artii::Base.new 
+    puts b.asciify(art)
+end
+
 
 for i in 0..sets - 1
-    puts a.asciify("SET #{i+1}")
+    ascii_art("SET #{i+1}")
     puts "Set #{i+1}"
+    sleep(2)
     timer(work_out_picks, work_out_length, work_out_dif)
 end
+
+ascii_art("GOOD WORKOUT")
+ascii_art("GO RECOVER")
